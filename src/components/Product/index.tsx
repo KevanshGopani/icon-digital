d5290dca939986a00b201837abe48075813dd39f
Kevansh <kevanshgopani5@gmail.com>
2025-02-16 11:09:52 +0530

    update brand api

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -556,7 +556,7 @@ const Products = () => {
                       </div>
 
                       {/* Dynamic Options */}
-                      {allCategories?.map((item: any, i: any) => (
+                      {allBrandsData?.map((item: any, i: any) => (
                         <div
                           key={i}
                           className="cursor-pointer px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700"
@@ -565,7 +565,7 @@ const Products = () => {
                             setIsOpen1(false);
                           }}
                         >
-                          {item?.title}
+                          {item?.name}
                         </div>
                       ))}
                     </div>
